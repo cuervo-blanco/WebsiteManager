008a78902bcfe40b4763b43eb20e61d1796dfa73
cuervo-blanco <jaios.228@gmail.com>
2024-01-18 15:19:12 -0500

    Implement delete image from Media Viewer through api request

--- app/utils/imageGetUtils.js
+++ app/utils/imageGetUtils.js
@@ -2,7 +2,6 @@
 export const getImageList = async (page, limit) => {
 
 	try {
-	console.log(`http://localhost:3002/image-list?page=${page}&limit=${limit}`);
 		const response = await fetch(`http://localhost:3002/image-list?page=${page}&limit=${limit}`);
 				if (!response.ok) {
 			throw new Error(`Image fetch failed: ${response.status}`);
@@ -12,4 +11,25 @@ export const getImageList = async (page, limit) => {
 		console.error('Error getting the image list:', error);
 		throw error;
 	}
+}
+
+export const deleteImage = async (imgUrl) => {
+	try {
+		const response = await fetch(`http://localhost:3002/delete-image`, {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({ imgUrl: imgUrl })
+	}
+);
+		if (!response.ok) {
+		throw new Error(`Image delete failed: ${response.status}`);
+		}
+
+	} catch (error){
+	console.error('Error deleting the image', error);
+		throw error;
+	}
+
 }
