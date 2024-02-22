0589af5e662aa68d206019efc8d992e696aea397
cuervo-blanco <jaios.228@gmail.com>
2024-02-22 13:04:48 -0500

    Updates to the blog page editor

--- app/admin-panel/blog/[post_id]/page.tsx
+++ app/admin-panel/blog/[post_id]/page.tsx
@@ -321,7 +321,7 @@ const handleSaveChanges = async () => {
 		<Editor
 			apiKey={editorKey}
 			init={{
-					plugins: 'tinycomments mentions anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount checklist mediaembed casechange export formatpainter pageembed permanentpen footnotes advtemplate advtable advcode editimage tableofcontents powerpaste tinymcespellchecker autocorrect a11ychecker typography inlinecss',
+					plugins: 'anchor autolink charmap codesample emoticons image link lists media searchreplace table visualblocks wordcount ',
 					toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | align lineheight | tinycomments | checklist numlist bullist indent outdent | emoticons charmap | removeformat | mediaSelector',
 					tinycomments_mode: 'embedded',
 					tinycomments_author: 'Author name',
